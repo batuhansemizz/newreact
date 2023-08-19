6a325d5ad84007c3571ccc2b6c7b5e1d139f0037
Batuhan Semiz <batuhansemiz52@gmail.com>
2023-08-19 22:52:23 +0300

    proje bitişi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import Course from './Course';
+import React from './img/indir (1).png'
+import Vue from './img/indir (2).png'
+import Angular from './img/indir (3).png'
+import Bootsrap from './img/indir.png'
+import 'bulma/css/bulma.css'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <section class="hero is-link">
+            <div class="hero-body">
+              <p class="title">
+                Kurslarım 
+              </p>
+           
+            </div>
+          </section>
+          <section className='section'>
+            <div className='container'>
+         <div className='columns'>
+           <div className='column'>
+               <Course 
+                image= {Angular}
+                title="Angular"
+                description="Angular, Google tarafından geliştirilen bir JavaScript çerçevesidir. Büyük ölçekli uygulamalar geliştirmek için kullanılır. Tek sayfa uygulamalar (SPA) oluşturmak için geniş bir özellik setine sahiptir. TypeScript tabanlıdır ve kesin tip denetimi sağlar. Zengin modülerlik, veri bağla"
+                
+              />
+           </div>
+           <div className='column'>
+                <Course
+                  image ={React}
+                  title="React:js"
+                  description="React, Facebook tarafından oluşturulan bir JavaScript kütüphanesidir. Kullanıcı arayüzleri oluşturmak için kullanılır. React, bileşen tabanlı bir yaklaşım sunar, bu da kullanıcı arayüzünü bağımsız bileşenlere ayırmanıza olanak tanır. Virtual DOM sayesinde verimli bir"
+                  />
+           </div>
+           <div className='column'>
+               <Course 
+                image={Vue}
+                title="Vue.JS"
+                description="Vue, hafif ve esnek bir JavaScript çerçevesidir. İki yönlü veri bağlama, bileşen tabanlı yapı gibi modern özellikleri basit ve anlaşılır bir şekilde sunar. Vue, yavaşça benimseyebilirsiniz, yani mevcut projelerinize kolayca entegre edebilirsiniz. Ayrıca, belirli bir parçayı "
+                />
+           </div>
+           <div className='column'>
+              <Course
+              image={Bootsrap}
+              title="Bootstrap"
+              description="Bootstrap, popüler bir açık kaynaklı CSS çerçevesidir ve web siteleri ile web uygulamalarının hızlı ve düzenli bir şekilde tasarlanmasını kolaylaştırmak için kullanılır. İlk olarak Twitter tarafından geliştirilmiş olup daha sonra geniş bir topluluk tarafından desteklenmeye başlamıştır"
+              />
+            
+           </div>
+         </div>
+     </div>
+          </section>
+     
+
+
+
+   
+    
+   
+    
     </div>
   );
 }
---
+++ src/Course.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+
+function Course({ image,title,description }) {
+  
+  return (
+    <div>
+
+<div className="card">
+  <div className="card-image">
+    <figure className="image is-4by3">
+      <img src= {image}alt="kursalrım"/>
+    </figure>
+  </div>
+  <div className="card-content">
+    <div className="media">
+     
+      <div className="media-content">
+        <p className="title is-4">{title}</p>
+       
+      </div>
+    </div>
+
+    <div className="content">
+     {description}
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Course
